a9850e9efa3cad1853d2e2d880ace2fe6345fc76
Claudio Zizza <claudio@budgegeria.de>
2023-06-03 21:30:39 +0200

    Create omit array keys sorter

---
+++ src/Sorter/Omit.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Budgegeria\IntlSort\Sorter;
+
+use function array_values;
+
+final class Omit implements Sorter
+{
+    public function __construct(private Sorter $sorter)
+    {
+    }
+
+    /** @inheritdoc */
+    public function sort(array $values): array
+    {
+        $values = $this->sorter->sort($values);
+
+        return array_values($values);
+    }
+}
---
+++ tests/Sorter/OmitTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Budgegeria\IntlSort\Tests\Sorter;
+
+use Budgegeria\IntlSort\Comparator\Comparator;
+use Budgegeria\IntlSort\Exception\IntlSortException;
+use Budgegeria\IntlSort\Sorter\Asc;
+use Budgegeria\IntlSort\Sorter\Omit;
+use Budgegeria\IntlSort\Sorter\Sorter;
+use Budgegeria\IntlSort\Tests\Collator;
+use PHPUnit\Framework\TestCase;
+
+class OmitTest extends TestCase
+{
+    use Collator;
+
+    public function testSort(): void
+    {
+        $comparator = $this->createMock(Sorter::class);
+        $comparator->expects(self::once())
+            ->method('sort')
+            ->with([2, 1])
+            ->willReturn([1 => 1, 0 => 2]);
+
+        $sorter = new Omit($comparator);
+
+        self::assertSame([1, 2], $sorter->sort([2, 1]));
+    }
+
+    public function testSortIntegration(): void
+    {
+        $sorter = new Omit(new Asc(new Comparator($this->createCollator())));
+
+        self::assertSame([1, 'a', 'b', 'c'], $sorter->sort(['b', 'c', 'a', 1]));
+    }
+
+    public function testSortThrowsException(): void
+    {
+        $sorter = $this->createStub(Sorter::class);
+        $sorter->method('sort')
+            ->willThrowException(IntlSortException::errorOnSort('error'));
+
+        $sorter = new Omit($sorter);
+
+        $this->expectException(IntlSortException::class);
+        $sorter->sort([1, 2]);
+    }
+}
