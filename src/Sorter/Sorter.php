d656c192a8b9159594f1f445d47f46179f7f6c87
Claudio Zizza <claudio@budgegeria.de>
2024-04-29 23:07:19 +0200

    Extend Sorter types with object type

--- src/Sorter/Sorter.php
+++ src/Sorter/Sorter.php
@@ -9,9 +9,9 @@ use Budgegeria\IntlSort\Exception\IntlSortException;
 interface Sorter
 {
     /**
-     * @param array<int|string, int|string|null> $values
+     * @param array<int|string, int|string|object|null> $values
      *
-     * @return array<int|string, int|string|null>
+     * @return array<int|string, int|string|object|null>
      *
      * @throws IntlSortException
      */
