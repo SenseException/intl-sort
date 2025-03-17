f95d174fa7bdd01dce9cf60a4bf147d88b60beb6
Claudio Zizza <claudio@budgegeria.de>
2025-03-17 22:59:44 +0100

    Create CallableAccess comparator test with invokable class

--- tests/Comparator/CallableAccessTest.php
+++ tests/Comparator/CallableAccessTest.php
@@ -14,12 +14,33 @@ use function is_array;
 /** @covers \Budgegeria\IntlSort\Comparator\CallableAccess */
 class CallableAccessTest extends TestCase
 {
-    public function testDelegateToCollator(): void
+    public function testCollatorWithFunction(): void
     {
         $func     = static function (mixed $value): string {
             assert(is_array($value));
 
             return (string) $value['foo'];
+        };
+        $collator = self::createMock(Collator::class);
+        $collator->expects(self::once())
+            ->method('compare')
+            ->with('1', '2')
+            ->willReturn(-1);
+
+        $comparator = new CallableAccess($collator, $func);
+
+        self::assertSame(-1, $comparator->compare(['foo' => '1'], ['foo' => '2']));
+    }
+
+    public function testCollatorWithClass(): void
+    {
+        $func     = new class {
+            public function __invoke(mixed $value): string
+            {
+                assert(is_array($value));
+
+                return (string) $value['foo'];
+            }
         };
         $collator = self::createMock(Collator::class);
         $collator->expects(self::once())
